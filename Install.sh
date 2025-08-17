a164ad5be585f2bdf61f3e4c7c6347d87da42527
raducksijaa <ronald.alan77@gmail.com>
2025-08-17 16:13:30 +0800

    Update Install.sh
    For firewall.user update

--- Install.sh
+++ Install.sh
@@ -2,15 +2,19 @@
 
 # Download controller
 wget -O /usr/lib/lua/luci/controller/ttl64.lua \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/lib/lua/luci/controller/ttl64.lua
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Fw3/usr/lib/lua/luci/controller/ttl64.lua
 
 # Download view
 wget -O /usr/lib/lua/luci/view/admin_status/ttl64.htm \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/lib/lua/luci/view/admin_status/ttl64.htm
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Fw3/usr/lib/lua/luci/view/admin_status/ttl64.htm
 
 # Download bin modipv4.sh
 wget -O /usr/bin/modipv4.sh \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/bin/modipv4.sh
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Fw3/usr/bin/modipv4.sh
+
+# Download firewall.user ttl fw3
+wget -O /etc/firewall.user \
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Fw3/etc/firewall.user
 
 # Bagi permission execute pada bin
 chmod +x /usr/bin/modipv4.sh
