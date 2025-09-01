8c278a5e174d8335f6468013904baaeae0917fc6
raducksijaa <ronald.alan77@gmail.com>
2025-09-01 13:09:26 +0800

    Update Install.sh ikut branch rnd

--- Install.sh
+++ Install.sh
@@ -2,19 +2,19 @@
 
 # Download controller
 wget -O /usr/lib/lua/luci/controller/ttl64.lua \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/lib/lua/luci/controller/ttl64.lua
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/RND/usr/lib/lua/luci/controller/ttl64.lua
 
 # Download view
 wget -O /usr/lib/lua/luci/view/admin_status/ttl64.htm \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/lib/lua/luci/view/admin_status/ttl64.htm
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/RND/usr/lib/lua/luci/view/admin_status/ttl64.htm
 
 # Download bin modipv4.sh
 wget -O /usr/bin/modipv4.sh \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/bin/modipv4.sh
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/RND/usr/bin/modipv4.sh
 
 # Download bin meodemtemp
 wget -O /usr/bin/modemtemp\
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/bin/modemtemp
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/RND/usr/bin/modemtemp
 
 # Bagi permission execute pada bin
 chmod +x /usr/bin/modipv4.sh
