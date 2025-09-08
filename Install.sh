a514785a716871171bd544a6c9bbb8f02b5aa4b9
raducksijaa <ronald.alan77@gmail.com>
2025-09-08 16:54:05 +0800

    Update Install.sh draft remove package

--- Install.sh
+++ Install.sh
@@ -16,6 +16,15 @@ https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Qwrt/usr/bin/modipv4.sh
 wget -O /usr/bin/modemtemp \
 https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Qwrt/usr/bin/modemtemp
 
+#delete unnecessaary interfaces
+uci -q delete network.wan6
+uci -q delete network.wan
+uci -q delete network.ipsec_server
+
+#delete pakage
+#opkg remove --force-removal-of-dependent-packages leigod-acc
+
+
 # Download firewall.user ttl fw3
 #wget -O /etc/firewall.user \
 #https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Fw3/etc/firewall.user
