3b90d4c0c57ddc5f924c541a2d5f27ec97c21dc3
raducksijaa <ronald.alan77@gmail.com>
2025-09-25 08:45:24 +0800

    Update Install.sh-ipv6

--- Install.sh
+++ Install.sh
@@ -1,29 +1,9 @@
 #!/bin/sh
 
-# Download controller
-wget -O /usr/lib/lua/luci/controller/ttl64.lua \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/lib/lua/luci/controller/ttl64.lua
-
-# Download view
-wget -O /usr/lib/lua/luci/view/admin_status/ttl64.htm \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/lib/lua/luci/view/admin_status/ttl64.htm
-
-# Download bin modipv4.sh
-wget -O /usr/bin/modipv4.sh \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/bin/modipv4.sh
-
-# Download bin meodemtemp
-wget -O /usr/bin/modemtemp \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/usr/bin/modemtemp
-
 # Download ttl64.nft
 wget -O /etc/nftables.d/ttl64.nft \
-https://raw.githubusercontent.com/Razifadm/3ModNssVpn/main/etc/nftables.d/ttl64.nft
+https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Ipv6yes/etc/nftables.d/ttl64.nft
 
-# Bagi permission execute pada bin
-chmod +x /usr/bin/modipv4.sh
-chmod +x /usr/bin/modemtemp
-
-echo "✅ 3Mod installed"
+echo "ipv6 0mbhotspot Installed"
 # Padam skrip ini sendiri
 rm -f "$0"
