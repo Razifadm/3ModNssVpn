ae84c19d1a76faf4158723c794486a1c94d0d40d
raducksijaa <ronald.alan77@gmail.com>
2025-09-25 08:45:45 +0800

    Update Uninstall.sh

--- Uninstall.sh
+++ Uninstall.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-#Remove controller
-rm -f /usr/lib/lua/luci/controller/ttl64.lua
-
-#Remove view
-rm -f /usr/lib/lua/luci/view/admin_status/ttl64.htm
-
-#Remove bin
-#rm -f /usr/bin/modipv4.sh
-#rm -f /usr/bin/modemtemp
 
 #Remove nftable rule
 #rm -f /etc/nftables.d/ttl64.nft
