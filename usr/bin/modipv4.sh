14ce4b3da36046025b3b18e4deaabc6e1b5e84b7
raducksijaa <ronald.alan77@gmail.com>
2025-09-26 06:31:21 +0800

    Update modipv4.sh change to nss and vpn mod

--- usr/bin/modipv4.sh
+++ usr/bin/modipv4.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-
-TTL_FILE="/etc/nftables.d/ttl64.nft"
-TTL_DISABLED="/etc/nftables.d/ttl64.nft.disabled"
-TTL_TABLE="ttl64"
 INTERFACE="qmodem_4_1"
 
 restart_services() {
@@ -23,24 +19,7 @@ restart_services() {
   logger -t TTL64 "DEBUG: Interface status: $(ifstatus $INTERFACE | grep -E '\"up\"|\"device\"')"
 }
 
-mod() {
-  [ -f "$TTL_DISABLED" ] && mv "$TTL_DISABLED" "$TTL_FILE"
-
-  uci set firewall.@defaults[0].flow_offloading='0'
-  uci set firewall.@defaults[0].flow_offloading_hw='0'
-  uci commit firewall >/dev/null 2>&1
-
-  uci set qmodem.'4_1'.pdp_type='ip'
-  uci commit qmodem
-
-  logger -t TTL64 "MODE: MOD (TTL active, Flow offloading disabled)"
-  restart_services
-}
-
 nss() {
-  [ -f "$TTL_FILE" ] && mv "$TTL_FILE" "$TTL_DISABLED" && \
-  nft delete table inet "$TTL_TABLE" >/dev/null 2>&1
-
   uci set firewall.@defaults[0].flow_offloading='1'
   uci set firewall.@defaults[0].flow_offloading_hw='1'
   uci commit firewall >/dev/null 2>&1
@@ -48,13 +27,11 @@ nss() {
   uci set qmodem.'4_1'.pdp_type='ipv4v6'
   uci commit qmodem
 
-  logger -t TTL64 "MODE: NSS (TTL removed, NSS enabled)"
+  logger -t TTL64 "MODE: NSS (ipv4v6 enable, NSS enabled)"
   restart_services
 }
 
 vpn() {
-  [ -f "$TTL_DISABLED" ] && mv "$TTL_DISABLED" "$TTL_FILE"
-
   uci set firewall.@defaults[0].flow_offloading='1'
   uci set firewall.@defaults[0].flow_offloading_hw='1'
   uci commit firewall >/dev/null 2>&1
@@ -68,8 +45,7 @@ vpn() {
 }
 
 case "$1" in
-  mod) mod ;;
   nss) nss ;;
   vpn) vpn ;;
-  *) echo "Usage: $0 {mod|nss|vpn}" ;;
+  *) echo "Usage: $0 {nss|vpn}" ;;
 esac
