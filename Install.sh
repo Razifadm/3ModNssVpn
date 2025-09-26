f2630a73f9a72bd647f53285ed0f584d83643f3f
raducksijaa <ronald.alan77@gmail.com>
2025-09-26 11:12:22 +0800

    Update Install.sh update

--- Install.sh
+++ Install.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+# Buang semua .nft dalam /etc/nftables.d
+rm -f /etc/nftables.d/*.nft
+
+#buang ttl64 lua dan htm
+rm -f /usr/lib/lua/luci/controller/ttl64.lua
+rm -f /usr/lib/lua/luci/view/admin_status/ttl64.htm
+#rm -f
+
 # Download ttl64.nft
 wget -O /etc/nftables.d/ttl64.nft \
 https://raw.githubusercontent.com/Razifadm/3ModNssVpn/Ipv6yes/etc/nftables.d/ttl64.nft
 
-echo "ipv6 0mbhotspot Installed"
-# Padam skrip ini sendiri
+uci set qmodem.'4_1'.pdp_type='ipv4v6'
+uci commit qmodem
+/etc/init.d/firewall restart >/dev/null 2>&1
+/etc/init.d/qmodem_network restart >/dev/null 2>&1
+
+echo "ipv6 0mbhotspot Installed, qmodem restarted"
+
+#Del
 rm -f "$0"
